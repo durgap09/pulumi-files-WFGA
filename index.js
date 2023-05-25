c4158a5efdcd611ea1ac4153c7bc2998f6f3ffe5
durgap09 <90268800+durgap09@users.noreply.github.com>
2023-05-25 15:27:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const pulumi = require("@pulumi/pulumi");
 const path = require("path");
 const node_modules_path = path.join(__dirname, "node_modules");
 const pulumiProgram = require("./pulumiProgram");
-const canvasJson = require("./canvas.json");
+const canvasJson = require("./jsonConfig.json");
 pulumiProgram(canvasJson, DepGraph, pulumi, node_modules_path)();
